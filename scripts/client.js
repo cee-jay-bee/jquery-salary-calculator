b594d371d1a816840dacb906204d4c019628c6b3
CJ Barnes <cristopher.barnes@gmail.com>
2021-09-04 09:48:06 -0500

    pushing information to the DOM updated

--- scripts/client.js
+++ scripts/client.js
@@ -12,20 +12,18 @@ const getEmployee = () => {
     let employeeID = $(`#employeeID`).val();
     let employeeTitle = $(`#employeeTitle`).val();
     let annualSalary = $(`#annualSalary`).val();
-    // empty out input field
-    $(`#firstName`).val();
-    $(`#lastName`).val();
-    $(`#employeeID`).val();
-    $(`#employeeTitle`).val();
-    $(`#annualSalary`).val();
-    // console log the value
-    showEmployee(firstName);
-    showEmployee(lastName);
-    showEmployee(employeeID);
-    showEmployee(employeeTitle);
-    showEmployee(annualSalary);
+    // empty out input fields
+    $(`#firstName`).val('');
+    $(`#lastName`).val('');
+    $(`#employeeID`).val('');
+    $(`#employeeTitle`).val('');
+    $(`#annualSalary`).val('');
+    // pass the input data to the function to put on the DOM
+    showEmployee(firstName, lastName, employeeID, employeeTitle, annualSalary);
 }
 
-const showEmployee = employeeInfo => {
-    console.log(employeeInfo);
+const showEmployee = (firstName, lastName, employeeID, employeeTitle, annualSalary) => {
+    console.log(firstName, lastName, employeeID, employeeTitle, annualSalary);
+    let el = $ ('#employeeTable');
+    el.append(`<tr><td>${firstName}</td><td>${lastName}</td><td>${employeeID}</td><td>${employeeTitle}</td><td>${annualSalary}</td><td><button id="deleteEmployee">Delete</button></td></tr>`);
 }
