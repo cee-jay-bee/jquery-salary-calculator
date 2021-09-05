eb5e1ba1dc01647d7b6fe6b761d83983a532e7bc
CJ Barnes <cristopher.barnes@gmail.com>
2021-09-04 19:29:11 -0500

    commented code and created ReadMe

--- README.md
+++ README.md
@@ -1,9 +1,24 @@
-# Project Name
-
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+# JQuery Salary Calculator
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
+This project is designed to track Salary expenses for your company. You can add any number of employees and track your current monthly salary based on annual salary costs.
+I started developing this project by creating a base input box for each needed field and stored the appropriate entry into variables so as to track the information entered.
+From there, I worked to append the inputted information onto the DOM and tracked the monthly salary to also append that information to the DOM.
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+## Usage
+
+- Input Employees First Name, Last Name, Employee ID, Title, and Annual Salary (use $XXXXX as format)
+- Click Submit to add them to the Table Below and Calculate Current Monthly Salary
+- Add as many employees as you have to calculate total monthly salary
+- If you need to remove an employee, click the appropriate 'Delete' button 
+
+## Built With
+
+- HTML, CSS, JS, JQuery
+
+## Acknowledgment
+
+- Thanks to Dev for his teaching and work to provide me with the knowledge to spin this project up and make it functional
+- Thanks to my wife, Theresa, for her support as I continue to learn and work on full stack development
+- Thanks to my classmates for the support that they offer when I have questions.
--- scripts/client.js
+++ scripts/client.js
@@ -1,14 +1,20 @@
+// onReady function to load when the page is loaded
 const onReady = () => {
-    console.log( 'JQ' );
+    // provides button operation that calls the getEmployee Function
     $( '#submitEmployeeButton').on( 'click', getEmployee );
+    // provides functionality to the added delete buttons in the table to remove the closest TR tags from the DOM
     $( `#employeeTable`).on( 'click', '#deleteEmployee', function(){$(this).closest('tr').remove();});
 }
 
+// calls the onReady function when the document is loaded
 $( document ).ready( onReady );
 
+// sets a global variable for tracking the totalMonthlySalary
 let totalMonthlySalary = 0;
 
+// getEmployee function that pulls information from the input fields and pushes those values to the showEmployee function
 const getEmployee = () => {
+    
     //target an element by ID and get its .val()
     let firstName = $(`#firstName`).val();
     let lastName = $(`#lastName`).val();
@@ -16,6 +22,7 @@ const getEmployee = () => {
     let employeeTitle = $(`#employeeTitle`).val();
     let annualSalary = $(`#annualSalary`).val();
 
+    // converts the input Annual Salary to a number type
     let numberSalary = Number(annualSalary.replace('$', ''));
 
     // empty out input fields
@@ -24,29 +31,36 @@ const getEmployee = () => {
     $(`#employeeID`).val('');
     $(`#employeeTitle`).val('');
     $(`#annualSalary`).val('');
-    // pass the input data to the function to put on the DOM
+
+    // pass the input data to the functions to put on the DOM
     showEmployee(firstName, lastName, employeeID, employeeTitle, annualSalary);
     monthlySalary(numberSalary);
 }
 
+// updates the Total monthly Salary on the DOM
 const monthlySalary = (numberSalary) => {
+    // empties anything currently in the monthly salary
     $( `#totalMonthlySalary`).empty();
+    // adds the current value of Total Monthly Salary to the passed annual salary parameter divided by 12 to get monthly salary
     $( `#totalMonthlySalary`).append(`\$${totalMonthlySalary += numberSalary/12}`);
+    // if the Total Monthly Salary value is greater than $20000, updates the background to be red and font to be white, otherwise removes those properties if it's below
     if (totalMonthlySalary > 20000){
         $( `#totalSalary`).css( `background-color` , `red`);
         $( `#totalSalary`).css( `color` , `white`);
+    } else {
+        $( `#totalSalary` ).removeProperty ( `background-color`);
+        $( `#totalSalary` ).removeProperty ( `color` );
     }
 }
 
-let employeeCounter = 0;
+// passes data gathered from getEmployee function to be able to append to the DOM
 const showEmployee = (firstName, lastName, employeeID, employeeTitle, annualSalary) => {
-    console.log(firstName, lastName, employeeID, employeeTitle, annualSalary);
-    employeeCounter++;
+    // gets the table element from HTML
     let el = $ ('#employeeTable');
-    el.append(`<tr class="employeeRow${employeeCounter}"><td>${firstName}</td><td>${lastName}</td><td>${employeeID}</td><td>${employeeTitle}</td><td>${annualSalary}</td><td><button id="deleteEmployee">Delete</button></td></tr>`);
+    //appends the relevant information for the input employee to the table as a new row with the different data points, as well as a button to delete the current employee
+    el.append(`<tr class="employeeRow"><td>${firstName}</td><td>${lastName}</td><td>${employeeID}</td><td>${employeeTitle}</td><td>${annualSalary}</td><td><button id="deleteEmployee">Delete</button></td></tr>`);
 }
 
 const deleteEmployee = () => {
-    console.log('In the deleteEmployee');
-    $( `.employeeRow` ).remove();
+    
 }
