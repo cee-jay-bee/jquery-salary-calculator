6edd8c9887347fab242d71c3316e9b81e0a1cd15
CJ Barnes <cristopher.barnes@gmail.com>
2021-09-04 19:10:26 -0500

    base mode completed

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,7 @@
 const onReady = () => {
     console.log( 'JQ' );
     $( '#submitEmployeeButton').on( 'click', getEmployee );
+    $( `#employeeTable`).on( 'click', '#deleteEmployee', function(){$(this).closest('tr').remove();});
 }
 
 $( document ).ready( onReady );
@@ -14,7 +15,9 @@ const getEmployee = () => {
     let employeeID = $(`#employeeID`).val();
     let employeeTitle = $(`#employeeTitle`).val();
     let annualSalary = $(`#annualSalary`).val();
-    let numberSalary = Number(annualSalary);
+
+    let numberSalary = Number(annualSalary.replace('$', ''));
+
     // empty out input fields
     $(`#firstName`).val('');
     $(`#lastName`).val('');
@@ -23,12 +26,27 @@ const getEmployee = () => {
     $(`#annualSalary`).val('');
     // pass the input data to the function to put on the DOM
     showEmployee(firstName, lastName, employeeID, employeeTitle, annualSalary);
-    $( `#totalMonthlySalary`).empty();
-    $( `#totalMonthlySalary`).append(`\$${totalMonthlySalary += numberSalary}`)
+    monthlySalary(numberSalary);
 }
 
+const monthlySalary = (numberSalary) => {
+    $( `#totalMonthlySalary`).empty();
+    $( `#totalMonthlySalary`).append(`\$${totalMonthlySalary += numberSalary/12}`);
+    if (totalMonthlySalary > 20000){
+        $( `#totalSalary`).css( `background-color` , `red`);
+        $( `#totalSalary`).css( `color` , `white`);
+    }
+}
+
+let employeeCounter = 0;
 const showEmployee = (firstName, lastName, employeeID, employeeTitle, annualSalary) => {
     console.log(firstName, lastName, employeeID, employeeTitle, annualSalary);
+    employeeCounter++;
     let el = $ ('#employeeTable');
-    el.append(`<tr><td>${firstName}</td><td>${lastName}</td><td>${employeeID}</td><td>${employeeTitle}</td><td>\$${annualSalary}</td><td><button id="deleteEmployee">Delete</button></td></tr>`);
+    el.append(`<tr class="employeeRow${employeeCounter}"><td>${firstName}</td><td>${lastName}</td><td>${employeeID}</td><td>${employeeTitle}</td><td>${annualSalary}</td><td><button id="deleteEmployee">Delete</button></td></tr>`);
+}
+
+const deleteEmployee = () => {
+    console.log('In the deleteEmployee');
+    $( `.employeeRow` ).remove();
 }
