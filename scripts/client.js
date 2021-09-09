3cde0e044814ef7cf812e747e88d19770b9b5f7f
CJ Barnes <cristopher.barnes@gmail.com>
2021-09-09 10:45:07 -0500

    changed tracking of employees to array

--- scripts/client.js
+++ scripts/client.js
@@ -11,19 +11,38 @@ $( document ).ready( onReady );
 
 // sets a global variable for tracking the totalMonthlySalary
 let totalMonthlySalary = 0;
+// sets a global variable of array of employees
+let employees = [];
 
 // getEmployee function that pulls information from the input fields and pushes those values to the showEmployee function
 const getEmployee = () => {
     
-    //target an element by ID and get its .val()
-    let firstName = $(`#firstName`).val();
-    let lastName = $(`#lastName`).val();
-    let employeeID = $(`#employeeID`).val();
-    let employeeTitle = $(`#employeeTitle`).val();
-    let annualSalary = $(`#annualSalary`).val();
+    // assign values inputted from input into an object describing the employee
+    let newEmployee = {
+        firstName: $(`#firstName`).val(),
+        lastName: $(`#lastName`).val(),
+        employeeID: $(`#employeeID`).val(),
+        employeeTitle: $(`#employeeTitle`).val(),
+        annualSalary: $(`#annualSalary`).val()
+    }
 
+    // adds new employee to the array of employees
+    employees.push(newEmployee);
+
+    // target the element to make the changes to
+    let el = $ ('#employeeTable');
+    el.empty();
+    //appends the relevant information for the input employee to the table as a new row with the different data points, as well as a button to delete the current employee
+    el.append(`<th>First Name</th><th>Last Name</th><th>ID</th><th>Title</th><th>Annual Salary</th><th></th>`);
+
+    // loops through the employee Array to add each submission to the DOM
+    for(let i = 0; i < employees.length; i++){
+        //appends the relevant information for the input employee to the table as a new row with the different data points, as well as a button to delete the current employee
+        el.append(`<tr class="employeeRow"><td>${employees[i].firstName}</td><td>${employees[i].lastName}</td><td class="employeeID">${employees[i].employeeID}</td>
+        <td>${employees[i].employeeTitle}</td><td class="employeeSalary">${employees[i].annualSalary}</td><td><button class="deleteEmployee">Delete</button></td></tr>`);
+    }
     // converts the passed Annual Salary to a number type
-    let numberSalary = Number(annualSalary.replace('$', ''));
+    let numberSalary = Number($(`#annualSalary`).val().replace('$', ''));
 
     // empty out input fields
     $(`#firstName`).val('');
@@ -32,8 +51,7 @@ const getEmployee = () => {
     $(`#employeeTitle`).val('');
     $(`#annualSalary`).val('');
 
-    // pass the input data to the functions to put on the DOM
-    showEmployee(firstName, lastName, employeeID, employeeTitle, annualSalary);
+    // pass the input data to the functions to put monthly Salary on the DOM
     monthlySalary(numberSalary);
 }
 
@@ -56,21 +74,27 @@ const monthlySalary = (numberSalary) => {
     }
 }
 
-// passes data gathered from getEmployee function to be able to append to the DOM
-const showEmployee = (firstName, lastName, employeeID, employeeTitle, annualSalary) => {
-    // gets the table element from HTML
-    let el = $ ('#employeeTable');
-    //appends the relevant information for the input employee to the table as a new row with the different data points, as well as a button to delete the current employee
-    el.append(`<tr class="employeeRow"><td>${firstName}</td><td>${lastName}</td><td>${employeeID}</td><td>${employeeTitle}</td><td class="employeeSalary">${annualSalary}</td><td><button class="deleteEmployee">Delete</button></td></tr>`);
-}
+
 
 function deleteEmployee () {
     // find the salary of the employee that was deleted from the table
     let foundSalary = $(this).parent().parent().children(`.employeeSalary`).text();
+   
     // change that salary to a number type
     foundSalary = Number(foundSalary.replace('$', ''));
+
     // pass that salary to the monthly Salary function as a negative number 
     monthlySalary(-foundSalary);
+
+    // find the ID of the deleted Employee
+    let deletedEmployeeID = $(this).parent().parent().children( `.employeeID`).text();
+
+    // loop through the employees to delete the one that matches the ID of the deleted employee
+    for (let i=0; i < employees.length; i++){
+        if (employees[i].employeeID === deletedEmployeeID){
+            employees.splice(i, 1);
+        }
+    }
     // when clicking a deleteEmployee button removes the nearest Table Row attribute as well as all children
     $( this ).closest('tr').remove();
 }
